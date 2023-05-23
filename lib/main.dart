ec7878061b41e628741cde901b7d6388ed4a95e0
unknown <zulfandarusalam@gmail.com>
2023-05-23 19:56:17 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http ;
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MaterialApp(
@@ -9,9 +9,8 @@ void main() {
   ));
 }
 
-
 class getApi extends StatefulWidget {
-  const getApi({super.key});
+  const getApi({Key? key}) : super(key: key);
 
   @override
   State<getApi> createState() => _getApiState();
@@ -19,37 +18,52 @@ class getApi extends StatefulWidget {
 
 class _getApiState extends State<getApi> {
   final Uri url = Uri.parse("https://reqres.in/api/users?per_page=15");
-  Future<List<dynamic>> _fecthDataUsers() async {
+
+  Future<List<dynamic>> _fetchDataUsers() async {
     var result = await http.get(url);
     return json.decode(result.body)['data'];
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('GETTING API'),
-        
+        title: Text('GETTING API BY GROUP 1'),
+        backgroundColor: Colors.deepPurple,
       ),
-      body:Container( 
+      body: Container(
         child: FutureBuilder<List<dynamic>>(
-          future: _fecthDataUsers(),
+          future: _fetchDataUsers(),
           builder: (BuildContext context, AsyncSnapshot snapshot) {
             if (snapshot.hasData) {
-              return ListView.builder(
-                  padding: EdgeInsets.all(10),
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        backgroundImage:
-                            NetworkImage(snapshot.data[index]['avatar']),
-                      ),
-                      title: Text(snapshot.data[index]['first_name'] + " " + snapshot.data[index]['last_name']),
-                      subtitle: Text(snapshot.data[index]['email']),
-                    );
-                  });
+              return ListView.separated(
+                padding: EdgeInsets.all(10),
+                itemCount: snapshot.data.length,
+                separatorBuilder: (BuildContext context, int index) =>
+                    Divider(), // Tambahkan Divider di antara setiap ListTile
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      backgroundImage:
+                          NetworkImage(snapshot.data[index]['avatar']),
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      side: BorderSide(color: Colors.black),
+                    ),
+                    title: Text(snapshot.data[index]['first_name'] +
+                        " " +
+                        snapshot.data[index]['last_name']),
+                    subtitle: Text(snapshot.data[index]['email']),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 16,
+                    ),
+                  );
+                },
+              );
             } else {
               return Center(child: CircularProgressIndicator());
             }
